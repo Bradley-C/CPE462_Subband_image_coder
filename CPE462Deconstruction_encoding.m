b8f135d4b3928740b111a830fb7e0a4d04d76ca4
Ryan O'Shea <rpo1202@gmail.com>
2019-05-01 14:55:06 -0400

    Working compression with wavelet decomp and quanitization

--- CPE462Deconstruction_encoding.m
+++ CPE462Deconstruction_encoding.m
@@ -3,10 +3,10 @@ close all;
 clear all; %#ok<*CLALL>
 
 % Input the image file
-X=uint8(imread('abstract.jpg'));
+X=uint8(imread('colorwheel.png'));
 
 % Create new image to compare to output
-imwrite(X,'in.jpg');
+imwrite(X,'in.png');
 [r,c,p] = size(X);
 
 % Seperate image by RGB colors
@@ -19,38 +19,73 @@ figure;
 imshow(X);
 title('Original image');
 
-% define the wavelet type and how many level of decomposition performed
-wavelet = 'haar';
-N = 1;
-[LoD,HiD] = wfilters (wavelet,'d'); 
+% define the decomposition wavelet type and how many levels thershhold
+% levels to use for quantization
+wavelet = 'haar'; %type of decompisition
+N=10; %levels to quantize image by (can be 1 to 20)
 
 % Perform a level 1 decomposition of the image along the RGB channels
-% [cA1,cH1,cV1,cD1] = dwt2(X1,wavelet);
-% [cA2,cH2,cV2,cD2] = dwt2(X2,wavelet);
-% [cA3,cH3,cV3,cD3] = dwt2(X3,wavelet);
-% [height, width] = size(cA1);
-%  rX1 = uint8(idwt2(qA1,qH1,qV1,qD1,wavelet));
-%  rX2 = uint8(idwt2(qA2,qH2,qV2,qD2,wavelet));
-%  rX3 = uint8(idwt2(qA3,qH3,qV3,qD3,wavelet));
-[C1,S1] = wavedec2(X1,N,LoD,HiD);
-[C2,S2] = wavedec2(X2,N,LoD,HiD);
-[C3,S3] = wavedec2(X3,N,LoD,HiD);
+[cA1,cH1,cV1,cD1] = dwt2(X1,wavelet);
+[cA2,cH2,cV2,cD2] = dwt2(X2,wavelet);
+[cA3,cH3,cV3,cD3] = dwt2(X3,wavelet);
+[height, width] = size(cA1);
 
-% Compress(quantize) each channel individually
-[thr1,sorh1,keepapp1] = ddencmp('cmp','wv',C1);
-[thr2,sorh2,keepapp2] = ddencmp('cmp','wv',C2);
-[thr3,sorh3,keepapp3] = ddencmp('cmp','wv',C3);
-[XC1,CXC1,LXC1,PERF1,PERFL1] = wdencmp('gbl',C1,S1,wavelet,N,thr1,sorh1,keepapp1);
-[XC2,CXC2,LXC2,PERF2,PERFL2] = wdencmp('gbl',C2,S2,wavelet,N,thr2,sorh2,keepapp2);
-[XC3,CXC3,LXC3,PERF3,PERFL3] = wdencmp('gbl',C3,S3,wavelet,N,thr3,sorh3,keepapp3);
+% Find N threshhold levels (only need to do one set since all sets share
+% range of values)
+threshA=multithresh(cA1,N);
+threshH=multithresh(cH1,N);
+threshV=multithresh(cV1,N);
+threshD=multithresh(cD1,N);
 
-% Break down the compressed wavlets by appox. horz. verit. & diag.
-cA1 = appcoef2(CXC1,LXC1,wavlet,N);
-[cH1,cV1,cD1] = detcoef2('all',CXC1,LXC1,N);
-cA2 = appcoef2(CXC2,LXC2,wavlet,N);
-[cH2,cV2,cD2] = detcoef2('all',CXC2,LXC2,N);
-cA3 = appcoef2(CXC3,LXC3,wavlet,N);
-[cH3,cV3,cD3] = detcoef2('all',CXC3,LXC3,N);
+% Contrusts a vector such that the min value in each quantization interval
+% is assigned to the N levels of the output image
+valuesMinA= [min(cA1(:)) threshA];
+valuesMinH= [min(cH1(:)) threshH];
+valuesMinV= [min(cV1(:)) threshD];
+valuesMinD= [min(cD1(:)) threshV];
+
+% Quantize image using N threshhold levels
+[qcA1, indexA1] = imquantize(cA1,threshA,valuesMinA);
+[qcH1, indexH1] = imquantize(cH1,threshH,valuesMinH);
+[qcV1, indexV1] = imquantize(cV1,threshV,valuesMinV);
+[qcD1, indexD1] = imquantize(cD1,threshD,valuesMinD);
+
+[qcA2, indexA2] = imquantize(cA2,threshA,valuesMinA);
+[qcH2, indexH2] = imquantize(cH2,threshH,valuesMinH);
+[qcV2, indexV2] = imquantize(cV2,threshV,valuesMinV);
+[qcD2, indexD2] = imquantize(cD2,threshD,valuesMinD);
+
+[qcA3, indexA3] = imquantize(cA3,threshA,valuesMinA);
+[qcH3, indexH3] = imquantize(cH3,threshH,valuesMinH);
+[qcV3, indexV3] = imquantize(cV3,threshV,valuesMinV);
+[qcD3, indexD3] = imquantize(cD3,threshD,valuesMinD);
+
+% to be used in the reconstruction REMOVE IN FINAL SUBMISSION
+rX1 = uint8(idwt2(qcA1,qcH1,qcV1,qcD1,wavelet));
+rX2 = uint8(idwt2(qcA2,qcH2,qcV2,qcD2,wavelet));
+rX3 = uint8(idwt2(qcA3,qcH3,qcV3,qcD3,wavelet));
+Q(:,:,1)=rX1;
+Q(:,:,2)=rX2;
+Q(:,:,3)=rX3;
+figure;
+imwrite(Q,'out.png');
+imshow(Q);
 
 % Add encoding here also make the output something you can decode into
 % other file           
+
+%Encode red? channel (X1)
+%Find the probability of each of the symbols (pixel values 0-255)
+XVec = X1(:);
+[p, symbols] = hist(XVec,double(unique(XVec)));
+p=p/sum(p);
+
+%symbols=symbols/sum(symbols);
+%Create a Huffman code dictionary using the symbols and probabilities we
+%just found
+[RedHuffDict,avglen]=huffmandict(symbols,p);
+
+%avglen is still like 7.9 so not too great and I'm not sure why
+
+encodedImg = huffmanenco(XVec, RedHuffDict);
+
